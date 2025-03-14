2a4724ba232d1ff9c3c3e062269956d79d8d8e57
mo <mcc3@student.unimelb.edu.au>
2025-03-14 22:11:46 +1100

    Refined background

--- styleAI/src/app/page.tsx
+++ styleAI/src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Header } from "@/components/home/header";
 import { Hero } from "@/components/home/content";
-import Background from "@/components/Background";
+import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#E6F4EA]">
       {/* <Header />
       <Hero /> */}
-      <Background />
+      <Homepage />
     </div>
   );
 }
