d03b3b51a823a5f0424dd76050e0254f9de9df7e
mo <mcc3@student.unimelb.edu.au>
2025-03-15 01:44:28 +1100

    Created title

--- styleAI/src/components/Homepage/index.tsx
+++ styleAI/src/components/Homepage/index.tsx
@@ -1,9 +1,13 @@
 import './Homepage.css';
+import Header from '../Header';
 
 export default function Homepage() {
   return (
-    <div className="fake-shader">
-      
+    <div className="homepage">
+      <div className="fake-shader"></div>
+      <div className="container">
+        <Header />
+      </div>
     </div>
   )
 }
