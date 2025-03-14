f41de763eebc418773c8e502cd88ce2f436bb42e
mo <mcc3@student.unimelb.edu.au>
2025-03-14 21:58:06 +1100

    Created background animation

--- styleAI/src/app/page.tsx
+++ styleAI/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Header } from "@/components/home/header";
 import { Hero } from "@/components/home/content";
+import Background from "@/components/Background";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#E6F4EA]">
-      <Header />
-      <Hero />
+      {/* <Header />
+      <Hero /> */}
+      <Background />
     </div>
   );
 }
